c825738e38628555477f4d9a0d2b1910479e9a3b
Alex Doutsinis <Alexiosdut@gmail.com>
2022-11-24 18:15:46 +0200

    implement a rate limiter to prevent spammers

--- src/api/functions/hello.ts
+++ src/api/functions/hello.ts
@@ -8,7 +8,3 @@ async function hello(event: APIGatewayEvent, context: Context) {
 }
 
 export const handler = globalMiddleware(hello, ["get"]);
-
-// todos:
-// 1. fix edge environment log message
-// 2. check 404 errors when hitting a function that doesn't exist
--- src/api/lib/middleware/globalMiddleware.ts
+++ src/api/lib/middleware/globalMiddleware.ts
@@ -2,6 +2,11 @@ import middy from "@middy/core";
 import httpJsonBodyParser from "@middy/http-json-body-parser";
 import httpEventNormalizer from "@middy/http-event-normalizer";
 import { Handler, APIGatewayEvent, Context, Callback } from "aws-lambda";
+import limiter from 'lambda-rate-limiter';
+
+const rateLimit = limiter({
+  interval: 60 * 1000
+}).check;
 
 function middleware(handler: Handler) {
   return middy(handler).use([httpJsonBodyParser(), httpEventNormalizer()]);
@@ -17,6 +22,22 @@ export default function globalMiddleware(handler: Handler, allowedHttpMethods: s
             }
         }
 
+        // ---- Security Layer ----
+        // 1. Config CORS
+        // 2. Check for custom header
+        // 3. Implement a rate limiter to prevent spammers
+
+        try {
+            const maxAmountOfRequestsAllowed = 10
+            const ip = event.headers["client-ip"] as string
+            await rateLimit(maxAmountOfRequestsAllowed, ip)
+        }
+        catch(ex) {
+            return { statusCode: 429 };
+        }
+        // ---- Security Layer ----
+
+        // Execute the actually function
         return middleware(handler)(event, context, callback)
     }
 }
